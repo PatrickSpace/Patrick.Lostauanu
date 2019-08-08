68b791384fc1547c84933cb0fc4ed0965cd02047
PatrickSpace <jplosta01@gmail.com>
2019-08-07 21:46:37 -0500

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-window.addEventListener('load', ()=> {
-    const preloader= document.querySelector('#cload');
+
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('#cload');
     preloader.classList.add("load-finish");
 })
+
+$(document).ready(function () {
+    document.getElementById("upid").onclick= function() {
+        $(window).scrollTop(0);
+    }
+    
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+            $( ".up" ).addClass( "up-act" );
+		} else {
+            $( ".up" ).removeClass( "up-act" );
+		}
+    });
+    
+
+});
+
