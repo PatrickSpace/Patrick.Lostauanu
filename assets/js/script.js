294e870aa45e387ee444dd50bd22a280f69d48fb
PatrickSpace <jplosta01@gmail.com>
2020-10-27 16:37:01 -0500

    backup

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,22 @@ window.addEventListener('load', () => {
 })
 
 $(document).ready(function () {
-    document.getElementById("upid").onclick= function() {
+    document.getElementById("upid").onclick = function () {
         $(window).scrollTop(0);
     }
-    
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-            $( ".up" ).addClass( "up-act" );
-		} else {
-            $( ".up" ).removeClass( "up-act" );
-		}
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $(".up").addClass("up-act");
+        } else {
+            $(".up").removeClass("up-act");
+        }
     });
-    
+
+    var largo = $('.imgproyecto').width();
+    var alto = largo * 56.25 / 100;
+    document.getElementsByClassName('imgproyecto').style.height = alto + "px";
+    alert(alto);
 
 });
 
